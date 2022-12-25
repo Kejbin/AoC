8232c7b21f1d8b9047bbbc0d8dd317e1d9c9b4d0
JAKUBT <s7325@horyzont.eu>
2022-12-25 19:35:06 +0100

    AOC_2022_13

---
+++ AOC_2022/AOC_2022/AOC_2022_13/Extensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AOC_2022_13
+{
+    internal static class Extensions
+    {
+        public static string[] SplitItems(this string s)
+        {
+            var l = new List<string>();
+
+            if (!s.Contains(",[") && !s.Contains("],"))
+                return new string[] { s };
+
+            while (s.Contains(",[") || s.Contains("],"))
+            {
+                var index = 0;
+
+                if (s.Contains("],"))
+                {
+                    index = s.IndexOf("],");
+                    s = s.Remove(index + 1, 1);
+                    l.Add(s.Substring(0, index + 1));
+                    s = s.Substring(index + 1);
+                }
+
+                if (s.Contains(",["))
+                {
+                    index = s.IndexOf(",[");
+                    s = s.Remove(index, 1);
+                    l.Add(s.Substring(0, index));
+                    s = s.Substring(index);
+                }
+            }
+
+            l.Add(s);
+
+            return l.ToArray();
+        }
+    }
+}
