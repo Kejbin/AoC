62d39c5c1d8693a6e16f53dee18de7bf4299a968
JAKUBT <s7325@horyzont.eu>
2023-12-07 18:34:58 +0100

    2023 Day 7 Part 2

--- AOC_2023/AOC_2023/DayWorkers/Day7.cs
+++ AOC_2023/AOC_2023/DayWorkers/Day7.cs
@@ -37,6 +37,23 @@ namespace AOC_2023.DayWorkers
             'A'
         };
 
+        private List<char> _oderedCardsPt2 = new List<char>
+        {            
+            'J',
+            '2',
+            '3',
+            '4',
+            '5',
+            '6',
+            '7',
+            '8',
+            '9',
+            'T',
+            'Q',
+            'K',
+            'A'
+        };
+
         public string Execute(string data)
         {
             var stopWatch = Stopwatch.StartNew();
@@ -44,13 +61,13 @@ namespace AOC_2023.DayWorkers
 
             var input = data.Split(Environment.NewLine).Where(s => !string.IsNullOrEmpty(s)).Select(s => s.Split(' ')).ToList();
 
-            return PartOne(input) + "\r\n" + PartTwo(input) + "\r\n" + $"Time: {stopWatch.ElapsedMilliseconds} ms";
+            return PartOne(input.ToList()) + "\r\n" + PartTwo(input.ToList()) + "\r\n" + $"Time: {stopWatch.ElapsedMilliseconds} ms";
         }
 
-        private int Compare(string v1, string v2)
+        private int Compare(string v1, string v2, bool part2)
         {
-            var r1 = GetSet(v1);
-            var r2 = GetSet(v2);
+            var r1 = GetSet(v1, part2);
+            var r2 = GetSet(v2, part2);
 
             if (r1 == r2)
             {
@@ -59,8 +76,8 @@ namespace AOC_2023.DayWorkers
                     if (v1[i] == v2[i])
                         continue;
 
-                    var p1 = Power(v1[i]);
-                    var p2 = Power(v2[i]);
+                    var p1 = Power(v1[i], part2);
+                    var p2 = Power(v2[i], part2);
 
                     if (p1 > p2)
                         return 1;
@@ -77,9 +94,9 @@ namespace AOC_2023.DayWorkers
             return -1;
         }
 
-        private int Power(char v) => _oderedCards.IndexOf(v);
+        private int Power(char v, bool part2) => part2 ? _oderedCardsPt2.IndexOf(v) : _oderedCards.IndexOf(v);
 
-        private Sets GetSet(string v)
+        private Sets GetSet(string v, bool part2)
         {
             Dictionary<char, int> chars = new ();
             foreach (var item in v)
@@ -95,29 +112,43 @@ namespace AOC_2023.DayWorkers
                 }
             }
 
-            Sets cardSet;
+            if (part2 && chars.TryGetValue('J', out int amount))
+            {
+                var orderedChars = chars.Where(w => w.Key != 'J').OrderByDescending(c => c.Value).ThenByDescending(o => _oderedCardsPt2.IndexOf(o.Key));
+
+                if (orderedChars.Any())
+                {
+                    var largest = orderedChars.First();
+                    chars.Remove('J');
+                    chars[largest.Key] += amount;
+                }
+            }
+
+            return GetSetValue(chars);
+        }
+
+        private Sets GetSetValue(Dictionary<char, int> chars)
+        {
             if (chars.Count == 5)
-                cardSet = Sets.HighCard;
+                return Sets.HighCard;
             else if (chars.Count == 4)
-                cardSet = Sets.OnePair;
+                return Sets.OnePair;
             else if (chars.Count == 3)
             {
                 if (chars.Any(c => c.Value > 2))
-                    cardSet = Sets.TheeOfAKind;
+                    return Sets.TheeOfAKind;
 
-                cardSet = Sets.TwoPair;
+                return Sets.TwoPair;
             }
             else if (chars.Count == 2)
             {
                 if (chars.Any(c => c.Value > 3))
-                    cardSet = Sets.FourOfAKind;
+                    return Sets.FourOfAKind;
 
-                cardSet = Sets.FullHouse;
+                return Sets.FullHouse;
             }
             else
-                cardSet = Sets.FiveOfAKind;
-
-            return cardSet;
+                return Sets.FiveOfAKind;
         }
 
         public string PartOne(object data)
@@ -125,7 +156,7 @@ namespace AOC_2023.DayWorkers
             int sum = 0;
             if (data is List<string[]> input)
             {
-                input.Sort((a, b) => Compare(a[0], b[0]));
+                input.Sort((a, b) => Compare(a[0], b[0], false));
 
                 for (int i = 1; i <= input.Count; i++)
                     sum += i * Convert.ToInt32(input[i - 1][1]);
@@ -139,12 +170,12 @@ namespace AOC_2023.DayWorkers
             int sum = 0;
             if (data is List<string[]> input)
             {
-                input.Sort((a, b) => Compare(a[0], b[0]));
+                input.Sort((a, b) => Compare(a[0], b[0], true));
 
                 for (int i = 1; i <= input.Count; i++)
                     sum += i * Convert.ToInt32(input[i - 1][1]);
             }
-
+          
             return $"Result Part 2: {sum}";
         }
     }
--- AOC_2023/AOC_2023/Program.cs
+++ AOC_2023/AOC_2023/Program.cs
@@ -6,9 +6,9 @@ if (!string.IsNullOrEmpty(dayNumber))
 {
     var day = DaySelector.GetDay(dayNumber);
 
-    //var path = Path.Combine(Environment.CurrentDirectory, "DayInputs", $"Day{dayNumber}.txt");
-    //var data = File.ReadAllText(path);
-    var data = "32T3K 765\r\nT55J5 684\r\nKK677 28\r\nKTJJT 220\r\nQQQJA 483";
+    var path = Path.Combine(Environment.CurrentDirectory, "DayInputs", $"Day{dayNumber}.txt");
+    var data = File.ReadAllText(path);
+    //var data = "32T3K 765\r\nT55J5 684\r\nKK677 28\r\nKTJJT 220\r\nQQQJA 483";
 
     if (!string.IsNullOrEmpty(data) && day != null)
     {
