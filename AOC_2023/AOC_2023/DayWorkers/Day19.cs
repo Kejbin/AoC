be3ea2018739b368966aaee793dd688fe95bf8d8
JAKUBT <s7325@horyzont.eu>
2023-12-19 21:25:25 +0100

    2023 Day 19 Part 1

--- AOC_2023/AOC_2023/DayWorkers/Day19.cs
+++ AOC_2023/AOC_2023/DayWorkers/Day19.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data;
 using System.Diagnostics;
+using System.IO;
 
 namespace AOC_2023.DayWorkers
 {
@@ -19,11 +20,11 @@ namespace AOC_2023.DayWorkers
                                 .Select(s  => s.Substring(1, s.Length - 2)
                                                .Split(',')
                                                .Select(ss => ss.Split('='))
-                                               .Select(sss => new Part { Category = sss[0], Value = Convert.ToInt32(sss[1]) })
+                                               .Select(sss => new Part { Category = sss[0][0], Value = Convert.ToInt32(sss[1]) })
                                                .ToList())
                                 .ToList();
 
-            return PartOne((rules, parts)) + "\r\n" + PartTwo(input) + "\r\n" + $"Time: {stopWatch.ElapsedMilliseconds} ms";
+            return PartOne((rules, parts)) + "\r\n" + PartTwo(rules) + "\r\n" + $"Time: {stopWatch.ElapsedMilliseconds} ms";
         }
 
         private List<Rule> SetRules(string[] strings)
@@ -37,7 +38,7 @@ namespace AOC_2023.DayWorkers
                 if (s.Length > 1)
                     rules.Add(new Rule { Category = s[0][0], Operator = s[0][1], Value = Convert.ToInt32(s[0].Substring(2, s[0].Length - 2)) , Result = s[1] });
                 else
-                    rules.Add(new Rule { Result = item });
+                    rules.Add(new Rule { Result = item.Substring(0, item.Length - 1) });
 
             }
 
@@ -49,7 +50,38 @@ namespace AOC_2023.DayWorkers
             int sum = 0;
             if (data is (Dictionary<string, List<Rule>> Rules, List<List<Part>> Parts))
             {
+                foreach (var part in Parts.ToList())
+                {
+                    string res = "in";
+                    var rule = Rules[res];
+                    while (res != "A" && res != "R")
+                    {
+                        for (int i = 0; i < rule.Count; i++)
+                        {
+                            res = rule[i].Result;
 
+                            if (i == rule.Count - 1)
+                                break;
+
+                            var partValue = part.First(p => p.Category == rule[i].Category).Value;
+                            if (rule[i].Operator == '>' && partValue > rule[i].Value)
+                                break;
+
+                            if (rule[i].Operator == '<' && partValue < rule[i].Value)
+                                break;             
+                        }
+
+                        //select new rule
+                        if (res != "A" && res != "R")
+                            rule = Rules[res];
+                    }
+
+                    if (res == "R")
+                        Parts.Remove(part);
+                }
+
+                sum += Parts.Select(s => s.Select(ss => ss.Value).Sum()).Sum();
+                //Sum parts
             }
 
             return $"Result Part 1: {sum}";
@@ -58,9 +90,9 @@ namespace AOC_2023.DayWorkers
         protected override string PartTwo(object data)
         {
             int sum = 0;
-            if (data is object obj)
+            if (data is Dictionary<string, List<Rule>> Rules)
             {
-
+                //TODO Work on ranges
             }
 
             return $"Result Part 2: {sum}";
@@ -77,7 +109,7 @@ namespace AOC_2023.DayWorkers
 
     public class Part
     {
-        public string Category { get; set; }
+        public char Category { get; set; }
         public int Value { get; set; }
     }
 }
