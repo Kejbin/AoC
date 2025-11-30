f72a055ab6b0c9ddb31142678c8238eee490acdb
JAKUBT <s7325@horyzont.eu>
2025-11-30 16:20:46 +0100

    Init 2025

---
+++ AOC_2025/AOC_2025/DayWorkers/Day.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC_2025.DayWorkers
+{
+    public abstract class Day
+    {
+        public abstract string Execute(string data);
+        protected abstract string PartOne(object data);
+        protected abstract string PartTwo(object data);
+    }
+}
---
+++ AOC_2025/AOC_2025/DayWorkers/Day1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC_2025.DayWorkers
+{
+    internal class Day1 : Day
+    {
+        public override string Execute(string data)
+        {
+            return PartOne(data) + "\r\n" + PartTwo(data);
+        }
+
+        protected override string PartOne(object data)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string PartTwo(object data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
