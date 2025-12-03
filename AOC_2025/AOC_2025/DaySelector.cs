a444d5c95c9dd5fbaa418f95a15cf3516039f557
Jakub Trojanowski <JTr@aucotec.com>
2025-12-03 12:36:39 +0100

    Day

--- AOC_2025/AOC_2025/DaySelector.cs
+++ AOC_2025/AOC_2025/DaySelector.cs
@@ -13,7 +13,7 @@ namespace AOC_2025
         {
             "1" => new Day1(),
             "2" => new Day2(),
-            //"3" => new Day3(),
+            "3" => new Day3(),
             //"4" => new Day4(),
             //"5" => new Day5(),
             //"6" => new Day6(),
